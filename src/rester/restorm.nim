c604ad91568d00d3fd43d4bff6b4c5818aedda00
Constantine Molchanov <moigagoo@live.com>
2019-01-24 23:37:15 +0400

    Reimplement insert proc.

--- src/rester/restorm.nim
+++ src/rester/restorm.nim
@@ -1,14 +1,11 @@
 import strutils, strformat, macros, sequtils, options
+import typetraits
 import db_sqlite
 
 import rowutils
-export rowutils
 
-
-template pk {.pragma.}
 
 template table(val: string) {.pragma.}
-
 template db(val: DbConn) {.pragma.}
 
 
@@ -45,40 +42,24 @@ proc getTable(T: type): string =
   else: T.name.toLowerAscii()
 
 template makeWithDbConn(connection, user, password, database: string,
-                        dbProcs: NimNode): untyped {.dirty.} =
+                        dbOthers: NimNode): untyped {.dirty.} =
   template withDbConn*(body: untyped): untyped {.dirty.} =
     block:
       let dbConn = open(connection, user, password, database)
 
-      proc all(T: type): seq[T] =
-        for row in dbConn.fastRows(sql"SELECT * FROM ?", T.getTable()):
-          result.add row.to(T)
-
-      proc one(T: type, id: int): T =
-        dbConn.getRow(sql"SELECT  * FROM ? WHERE id = ?", T.getTable(), id).to(T)
-
-      proc where(T: type, clause: string): seq[T] =
-        for row in dbConn.fastRows(sql &"SELECT * FROM ? WHERE {clause}", T.getTable()):
-          result.add row.to(T)
-
       proc insert(obj: var object) =
-        var fields: seq[string]
+        var fields, values: seq[string]
 
-        for field, _ in obj.fieldPairs: fields.add field
+        for field, value in obj.fieldPairs:
+          if field != "id":
+            fields.add field
+            values.add $value
 
-        let query = sql "INSERT INTO $# ($#) VALUES ($#)" %
-                    [type(obj).getTable(), fields.join(","), '?'.repeat(len(fields))]
+        let query = sql "INSERT INTO ? ($1) VALUES ($1)" % '?'.repeat(fields.len).join(",")
 
-        obj.id = dbConn.insertId(query, obj.toRow()).int
+        obj.id = dbConn.insertID(query, obj.type.getTable() & fields & values).int
 
-      proc delete(obj: var object) =
-        dbConn.exec(
-          sql"DELETE FROM ? WHERE id = ?",
-          type(obj).getTable(), obj.id
-        )
-        obj.id = 0
-
-      dbProcs
+      dbOthers
 
       try: body
       finally: dbConn.close()
@@ -89,32 +70,30 @@ macro db*(connection, user, password, database: string, body: untyped): untyped 
 
   var
     dbTypes = newStmtList()
-    dbProcs = newStmtList()
+    dbOthers = newStmtList()
 
   for node in body:
-    expectKind(node, {nnkTypeSection, nnkProcDef})
-
-    if node.kind == nnkProcDef:
-      dbProcs.add node
-
-    else:
+    if node.kind == nnkTypeSection:
       dbTypes.add node
 
-  result.add getAst makeWithDbConn(connection, user, password, database, dbProcs)
+    else:
+      dbOthers.add node
+
+  result.add getAst makeWithDbConn(connection, user, password, database, dbOthers)
   result.add dbTypes
 
 
 db("rester.db", "", "", ""):
   type
     User {.table: "users".} = object
-      id {.pk.}: int
+      id: int
       email: string
       age: int
     Book {.table: "books".} = object
-      id {.pk.}: int
+      id: int
       title: string
     Edition {.table: "editions".} = object
-      id {.pk.}: int
+      id: int
       title: string
       bookId: int
     UserBook = object
@@ -154,19 +133,20 @@ db("rester.db", "", "", ""):
 
 when isMainModule:
   withDbConn:
-    for i in 1..10:
-      var user = User(email: "asd@asd.asd", age: i)
-      user.insert()
+    var u = User(email: "user@example.com", age: 23)
+    u.insert()
 
-    echo User.all
+    echo u
 
-  withDbConn:
-    var users = User.all
-    for user in users.filterIt(it.email=="asd@asd.asd").mitems:
-      user.delete()
+  #   echo User.all
 
-    echo User.all()
-    # var usersAge1 = User.getWhere("age=1")
-    # for user in usersAge1.mitems:
-    #   user.delete()
-    # echo User.getWhere("age=1")
+  # withDbConn:
+  #   var users = User.all
+  #   for user in users.filterIt(it.email=="asd@asd.asd").mitems:
+  #     user.delete()
+
+  #   echo User.all()
+  #   # var usersAge1 = User.getWhere("age=1")
+  #   # for user in usersAge1.mitems:
+  #   #   user.delete()
+  #   # echo User.getWhere("age=1")
