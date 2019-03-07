233ecacd8c38c89dc078eacb2de79c6c4b3ceed9
Constantine Molchanov <moigagoo@live.com>
2019-03-07 13:03:00 +0400

    Add docstring.

--- src/norm/objutils.nim
+++ src/norm/objutils.nim
@@ -179,6 +179,8 @@ proc toTypeDef*(obj: ObjRepr): NimNode =
   )
 
 proc getByName*[T: ObjRepr | FieldRepr](reprs: openarray[T], name: string): T =
+  ## Get an ``ObjRepr`` or ``FieldRepr`` from an openarray by its name.
+
   for repr in reprs:
     if repr.signature.name == name:
       return repr
