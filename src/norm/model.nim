db3a53c360bc097bc4847b549e6f0c1675e067b4
Constantine Molchanov <moigagoo@live.com>
2020-05-16 16:49:27 +0400

    Add files.

---
+++ src/norm/model.nim
@@ -0,0 +1,55 @@
+import strutils
+import macros
+
+import pragmas
+
+
+type
+  Model* = object of RootObj
+    id* {.pk, ro.}: int
+
+
+macro dot*(obj: object, fieldName: string): untyped =
+  ## Access object field value by name: ``obj.dot("field")`` translates to ``obj.field``.
+
+  newDotExpr(obj, newIdentNode(fieldName.strVal))
+
+proc tableName*(T: typedesc[Model]): string =
+  when T.hasCustomPragma(dbTable):
+    T.getCustomPragmaVal(dbTable)
+  else:
+    $T
+
+template colName*[T: Model](obj: T, fld: string): string =
+  when obj.dot(fld).hasCustomPragma(dbCol):
+    obj.dot(fld).getCustomPragmaVal(dbCol)
+  else:
+    fld
+
+template fullColName*[T: Model](obj: T, fld: string): string =
+  "$#.$#" % [T.tableName, obj.colName(fld)]
+
+proc colNames*[T: Model](obj: T): seq[string] =
+  for fld, val in obj.fieldPairs:
+    when val is Model:
+      result.add val.colNames
+    else:
+      result.add obj.colName(fld)
+
+proc nroColNames*[T: Model](obj: T): seq[string] =
+  for fld, val in obj.fieldPairs:
+    when not obj.dot(fld).hasCustomPragma(ro):
+      result.add obj.colName(fld)
+
+proc fullColNames*[T: Model](obj: T): seq[string] =
+  for fld, val in obj.fieldPairs:
+    when val is Model:
+      result.add val.fullColNames
+    else:
+      result.add obj.fullColName(fld)
+
+proc joinGroups*[T: Model](obj: T): seq[tuple[tbl, lftFld, rgtFld: string]] =
+  for fld, val in obj.fieldPairs:
+    when val is Model:
+      result.add (tbl: typeof(val).tableName, lftFld: obj.fullColName(fld), rgtFld: val.fullColName("id"))
+      result.add val.joinGroups
