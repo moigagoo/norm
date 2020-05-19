735d29d566a8809809abb5f1e206203f941fda8d
Constantine Molchanov <moigagoo@live.com>
2020-05-19 16:12:47 +0400

    Remove unused pragmas.

--- src/norm/pragmas.nim
+++ src/norm/pragmas.nim
@@ -16,33 +16,3 @@ template ro* {.pragma.}
 
   ``id`` field is ``ro`` by default.
   ]##
-
-template dbCol*(name: string) {.pragma.}
-  ##[ Name of the column generated for a ``norm.Model`` field.
-
-  If not set, the field name is used.
-  ]##
-
-template dbType*(val: string) {.pragma.}
-  ##[ DB type to use in table schema for this field.
-
-  If not set, matching type from ``norm.sqlite.dbtypes`` is used.
-  ]##
-
-template check*(cond: string) {.pragma.}
-  ## Add ``CHECK <CONDITION>`` constraint.
-
-template unique* {.pragma.}
-  ## Add ``UNIQUE`` constraint.
-
-template onDelete*(polc: string) {.pragma.}
-  ## Add ``ON DELETE <POLICY>`` constraint.
-
-template onUpdate*(polc: string) {.pragma.}
-  ## Add ``ON UPDATE <POLICY>`` constraint.
-
-template dbTable*(name: string) {.pragma.}
-  ##[ Table name.
-
-  If not set, the type name is used.
-  ]##
