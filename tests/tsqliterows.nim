f043dcc1a2d0b4f77344c04dc111a8db9704c103
Constantine Molchanov <moigagoo@live.com>
2020-05-20 11:47:23 +0400

    Add tests for row insertion.

---
+++ tests/tsqliterows.nim
@@ -0,0 +1,82 @@
+import unittest
+import std/with
+import os
+import strutils
+
+import norm/[model, sqlite]
+
+
+const dbFile = "test.db"
+
+
+suite "Row CRUD":
+  type
+    Toy = object of Model
+      price: float
+
+    Pet = object of Model
+      species: string
+      favToy: Toy
+
+    Person = object of Model
+      name: string
+      pet: Pet
+
+  func initToy(price: float): Toy = Toy(price: price)
+
+  func initPet(species: string, favToy: Toy): Pet =
+    Pet(species: species, favToy: favToy)
+
+  func initPerson(name: string, pet: Pet): Person =
+    Person(name: name, pet: pet)
+
+  setup:
+    removeFile dbFile
+
+    let
+      dbConn = open(dbFile, "", "", "")
+
+    var
+      toy = initToy(123.45)
+      pet = initPet("cat", toy)
+      person = initPerson("Alice", pet)
+
+    with dbConn:
+      createTables(person)
+
+  teardown:
+    close dbConn
+    removeFile dbFile
+
+  test "Insert row":
+    with dbConn:
+      insert toy
+
+    check toy.id > 0
+
+    let rows = dbConn.getAllRows(sql"SELECT price, id FROM Toy")
+
+    check rows.len == 1
+    check rows[0] == @[?123.45, ?toy.id]
+
+  test "Insert rows":
+    with dbConn:
+      insert person
+
+    check person.id > 0
+    check person.pet.id > 0
+    check person.pet.favToy.id > 0
+
+    let
+      personRows = dbConn.getAllRows(sql"SELECT name, pet, id FROM Person")
+      petRows = dbConn.getAllRows(sql"SELECT species, favToy, id FROM Pet")
+      toyRows = dbConn.getAllRows(sql"SELECT price, id FROM Toy")
+
+    check personRows.len == 1
+    check personRows[0] == @[?"Alice", ?person.pet.id, ?person.id]
+
+    check petRows.len == 1
+    check petRows[0] == @[?"cat", ?person.pet.favToy.id, ?person.pet.id]
+
+    check toyRows.len == 1
+    check toyRows[0] == @[?123.45, ?person.pet.favToy.id]
