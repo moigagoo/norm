576fa1338fbdfb536fe645a18093de8f5a6fd39a
Constantine Molchanov <moigagoo@live.com>
2020-05-25 17:05:17 +0400

    Update test.

--- tests/tmodel.nim
+++ tests/tmodel.nim
@@ -7,7 +7,7 @@ import models
 
 suite "Getting table and columns from Model":
   test "Table":
-    check Person.table == "'Person'"
+    check Person.table == """"Person""""
 
   test "Columns":
     let
@@ -21,11 +21,11 @@ suite "Getting table and columns from Model":
     check person.cols == @["name", "pet"]
     check person.cols(force = true) == @["name", "pet", "id"]
 
-    check person.fCol("name") == "'Person'.name"
-    check person.pet.fCol("species") == "'Pet'.species"
+    check person.fCol("name") == """"Person".name"""
+    check person.pet.fCol("species") == """"Pet".species"""
 
-    check person.rfCols == @["'Person'.name", "'Pet'.species", "'Toy'.price", "'Toy'.id", "'Pet'.id", "'Person'.id"]
-    check toy.rfCols == @["'Toy'.price", "'Toy'.id"]
+    check person.rfCols == @[""""Person".name""", """"Pet".species""", """"Toy".price""", """"Toy".id""", """"Pet".id""", """"Person".id"""]
+    check toy.rfCols == @[""""Toy".price""", """"Toy".id"""]
 
   test "Join groups":
     let
@@ -33,4 +33,4 @@ suite "Getting table and columns from Model":
       pet = initPet("cat", toy)
       person = initPerson("Alice", pet)
 
-    check person.joinGroups == @[("'Pet'", "'Person'.pet", "'Pet'.id"), ("'Toy'", "'Pet'.favToy", "'Toy'.id")]
+    check person. joinGroups == @[(""""Pet"""", """"Person".pet""", """"Pet".id"""), (""""Toy"""", """"Pet".favToy""", """"Toy".id""")]
