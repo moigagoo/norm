e129dea2aac189c2e477449256111773aebd8bab
Constantine Molchanov <moigagoo@live.com>
2020-05-25 17:05:32 +0400

    Remove dropTables tests.

--- tests/tsqlitetables.nim
+++ tests/tsqlitetables.nim
@@ -1,5 +1,4 @@
 import unittest
-import std/with
 import os
 import strutils
 
@@ -11,7 +10,7 @@ import models
 const dbFile = "test.db"
 
 
-suite "Table creation and dropping":
+suite "Table creation":
   setup:
     removeFile dbFile
     let dbConn = open(dbFile, "", "", "")
@@ -26,13 +25,6 @@ suite "Table creation and dropping":
     dbConn.createTables(toy)
 
     let qry = "PRAGMA table_info($#);"
-
-    check dbConn.getAllRows(sql qry % "Toy") == @[
-      @[?0, ?"price", ?"FLOAT", ?1, ?nil, ?0],
-      @[?1, ?"id", ?"INTEGER", ?1, ?nil, ?1],
-    ]
-
-    dbConn.createTables(toy, force = true)
 
     check dbConn.getAllRows(sql qry % "Toy") == @[
       @[?0, ?"price", ?"FLOAT", ?1, ?nil, ?0],
@@ -65,47 +57,3 @@ suite "Table creation and dropping":
       @[?1, ?"pet", ?"INTEGER", ?1, ?nil, ?0],
       @[?2, ?"id", ?"INTEGER", ?1, ?nil, ?1],
     ]
-
-    dbConn.createTables(person, force = true)
-
-    check dbConn.getAllRows(sql qry % "Toy") == @[
-      @[?0, ?"price", ?"FLOAT", ?1, ?nil, ?0],
-      @[?1, ?"id", ?"INTEGER", ?1, ?nil, ?1],
-    ]
-
-    check dbConn.getAllRows(sql qry % "Pet") == @[
-      @[?0, ?"species", ?"TEXT", ?1, ?nil, ?0],
-      @[?1, ?"favToy", ?"INTEGER", ?1, ?nil, ?0],
-      @[?2, ?"id", ?"INTEGER", ?1, ?nil, ?1],
-    ]
-
-    check dbConn.getAllRows(sql qry % "Person") == @[
-      @[?0, ?"name", ?"TEXT", ?1, ?nil, ?0],
-      @[?1, ?"pet", ?"INTEGER", ?1, ?nil, ?0],
-      @[?2, ?"id", ?"INTEGER", ?1, ?nil, ?1],
-    ]
-
-  test "Drop table":
-    let toy = initToy(123.45)
-
-    with dbConn:
-      createTables(toy)
-      dropTables(toy)
-
-    expect DbError:
-      dbConn.exec sql "SELECT NULL FROM Toy"
-
-  test "Drop tables":
-    let
-      toy = initToy(123.45)
-      pet = initPet("cat", toy)
-      person = initPerson("Alice", pet)
-
-    with dbConn:
-      createTables(person)
-      dropTables(person)
-
-    expect DbError:
-      dbConn.exec sql "SELECT NULL FROM Toy"
-      dbConn.exec sql "SELECT NULL FROM Pet"
-      dbConn.exec sql "SELECT NULL FROM Person"
