b6478deb3084f076301dbd2cc56ff399983abb7c
Constantine Molchanov <moigagoo@live.com>
2020-07-08 00:34:16 +0400

    Tests: Add tests for env cfg.

---
+++ tests/tpostgresenv.nim
@@ -0,0 +1,70 @@
+import unittest
+import os
+import strutils
+
+import norm/[model, postgres]
+
+import models
+
+
+const
+  dbHost = "postgres"
+  dbUser = "postgres"
+  dbPassword = "postgres"
+  dbDatabase = "postgres"
+
+
+suite "DB config from environment variables":
+  proc resetDb =
+    let dbConn = open(dbHost, dbUser, dbPassword, "template1")
+    dbConn.exec(sql "DROP DATABASE IF EXISTS $#" % dbDatabase)
+    dbConn.exec(sql "CREATE DATABASE $#" % dbDatabase)
+    close dbConn
+
+  setup:
+    resetDb()
+
+    putEnv(dbHostEnv, dbHost)
+    putEnv(dbUserEnv, dbUser)
+    putEnv(dbPassEnv, dbPassword)
+    putEnv(dbNameEnv, dbDatabase)
+
+  teardown:
+    delEnv(dbHostEnv)
+    delEnv(dbUserEnv)
+    delEnv(dbPassEnv)
+    delEnv(dbNameEnv)
+
+    resetDb()
+
+  test "Explicit DB connection":
+    let db = getDb()
+
+    defer:
+      close db
+
+    db.createTables(newToy())
+
+    let qry = sql """SELECT column_name::text, data_type::text
+      FROM information_schema.columns
+      WHERE table_name = $1
+      ORDER BY column_name"""
+
+    check db.getAllRows(qry, "Toy") == @[
+      @[?"id", ?"integer"],
+      @[?"price", ?"real"]
+    ]
+
+  test "Implicit DB connection":
+    withDb:
+      db.createTables(newToy())
+
+      let qry = sql """SELECT column_name::text, data_type::text
+        FROM information_schema.columns
+        WHERE table_name = $1
+        ORDER BY column_name"""
+
+      check db.getAllRows(qry, "Toy") == @[
+        @[?"id", ?"integer"],
+        @[?"price", ?"real"]
+      ]
