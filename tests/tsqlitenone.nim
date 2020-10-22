77256545b8499be2e2cdcdcb02ac6ef1003270e5
Constantine Molchanov <moigagoo@live.com>
2020-10-23 00:53:05 +0400

    Add test for NULL foreign keys.

--- tests/tsqlitenone.nim
+++ tests/tsqlitenone.nim
@@ -11,7 +11,7 @@ import models
 const dbFile = "test.db"
 
 
-suite "none Model field handling":
+suite "``NULL`` foreign keys":
   setup:
     removeFile dbFile
 
@@ -21,66 +21,15 @@ suite "none Model field handling":
     close dbConn
     removeFile dbFile
 
-  test "Create tables":
-    dbConn.createTables(newPet())
-    dbConn.createTables(newPerson("", none Pet))
-
-    let rows = dbConn.getAllRows(sql "SELECT name FROM sqlite_master WHERE type = 'table'")
-
-    check rows.len == 3
-    check @[?"Person"] in rows
-
-  test "Insert row":
-    dbConn.createTables(newPerson())
-
-    var person = newPerson("Alice", none Pet)
-
-    dbConn.insert(person)
-
-    let rows = dbConn.getAllRows(sql"SELECT name, pet FROM Person")
-
-    check rows.len == 1
-    check rows[0] == @[?"Alice", ?nil]
-
-  test "Get row":
+  test "Get row, nested models, NULL foreign key, container is ``some Model``":
     dbConn.createTables(newPerson())
 
     var
       inpPerson = newPerson("Alice", none Pet)
-      outPerson = newPerson("", none Pet)
+      outPerson = newPerson("", newPet())
 
     dbConn.insert(inpPerson)
 
     dbConn.select(outPerson, "Person.id = ?", inpPerson.id)
 
     check outPerson === inpPerson
-
-  test "Update row":
-    dbConn.createTables(newPerson())
-
-    var
-      inpPerson = newPerson("Alice", none Pet)
-      outPerson = newPerson()
-      pet = newPet("cat", newToy(123.45))
-
-    dbConn.insert(inpPerson)
-    dbConn.insert(pet)
-
-    inpPerson.pet = some pet
-    dbConn.update(inpPerson)
-
-    dbConn.select(outPerson, "Person.id = ?", inpPerson.id)
-
-    check outPerson === inpPerson
-
-  test "Delete row":
-    dbConn.createTables(newPerson())
-
-    var person = newPerson("Alice", none Pet)
-
-    dbConn.insert(person)
-    dbConn.delete(person)
-
-    expect KeyError:
-      discard newPerson().dup:
-        dbConn.select("Person.name = ?", "Alice")
