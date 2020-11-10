756664d3ea60aaaf57832c699fde598e35ac1f24
Constantine Molchanov <moigagoo@live.com>
2020-11-10 16:38:49 +0400

    Add test for fk pragma with invalid field type.

---
+++ tests/postgres/tfkpragmanotint.nim
@@ -0,0 +1,58 @@
+discard """
+  action: "reject"
+"""
+
+import strutils
+import unittest
+import times
+import os
+
+import norm/[model, pragmas, sqlite]
+
+
+const
+  dbHost = "postgres"
+  dbUser = "postgres"
+  dbPassword = "postgres"
+  dbDatabase = "postgres"
+
+
+type
+  User* = ref object of Model
+    lastLogin*: DateTime
+
+  Customer* = ref object of Model
+    userId* {.fk: User.}: float
+    email*: string
+
+
+proc newUser*(): User = User(lastLogin: now())
+
+proc newCustomer*(userId: int, email: string): Customer =
+  Customer(userId: userId, email: email)
+
+proc newCustomer*(userId: int): Customer =
+  newCustomer(userId, "")
+
+proc newCustomer*(): Customer =
+  newCustomer(newUser().id)
+
+
+suite "``fk`` pragma: non-``SomeInteger`` field":
+  proc resetDb =
+    let dbConn = open(dbHost, dbUser, dbPassword, "template1")
+    dbConn.exec(sql "DROP DATABASE IF EXISTS $#" % dbDatabase)
+    dbConn.exec(sql "CREATE DATABASE $#" % dbDatabase)
+    close dbConn
+
+  setup:
+    resetDb()
+    let dbConn = open(dbHost, dbUser, dbPassword, dbDatabase)
+
+  teardown:
+    close dbConn
+    resetDb()
+
+  test "Create table":
+    dbConn.createTables(newUser())
+    dbConn.createTables(newCustomer())
