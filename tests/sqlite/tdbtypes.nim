06d62cdba07f1dbf94c987ed5e335db7ce4a5754
Constantine Molchanov <moigagoo@live.com>
2021-01-31 10:25:24 +0300

    Add support for StringOfCap and PaddedStringOfCap for SQLite.

--- tests/sqlite/tdbtypes.nim
+++ tests/sqlite/tdbtypes.nim
@@ -1,9 +1,11 @@
 import unittest
 import os
+import strutils
 import times
 import sugar
+import std/with
 
-import norm/[model, sqlite]
+import norm/[model, sqlite, types]
 
 import ../models
 
@@ -18,6 +20,8 @@ suite "Import dbTypes from norm/private/sqlite/dbtypes":
     let dbConn = open(dbFile, "", "", "")
 
     dbConn.createTables(newUser())
+    dbConn.createTables(newNumber())
+    dbConn.createTables(newString())
 
   teardown:
     close dbConn
@@ -28,3 +32,74 @@ suite "Import dbTypes from norm/private/sqlite/dbtypes":
       dbConn.select("""lastLogin <= ?""", ?now())
 
     check len(users) == 0
+
+  test "Flavors of ``int``, create table":
+    let qry = "PRAGMA table_info($#);"
+
+    check dbConn.getAllRows(sql qry % "Number") == @[
+      @[?0, ?"n", ?"INTEGER", ?1, ?nil, ?0],
+      @[?1, ?"n16", ?"INTEGER", ?1, ?nil, ?0],
+      @[?2, ?"n32", ?"INTEGER", ?1, ?nil, ?0],
+      @[?3, ?"n64", ?"INTEGER", ?1, ?nil, ?0],
+      @[?4, ?"id", ?"INTEGER", ?1, ?nil, ?1]
+    ]
+
+  test "Flavors of ``int``, insert row":
+    var number = newNumber(1, 2'i16, 3'i32, 4'i64)
+
+    dbConn.insert(number)
+
+    check number.id > 0
+
+    let rows = dbConn.getAllRows(sql"""SELECT n, n16, n32, n64, id FROM "Number"""")
+
+    check rows.len == 1
+    check rows[0] == @[?1, ?2'i16, ?3'i32, ?4'i64, ?number.id]
+
+  test "Flavors of ``int``, get row":
+    var
+      inpNumber = newNumber(1, 2'i16, 3'i32, 4'i64)
+      outNumber = newNumber()
+
+    with dbConn:
+      insert(inpNumber)
+      select(outNumber, "n = $1", 1)
+
+    check inpNumber === outNumber
+
+  test "Flavors of ``string``, create table":
+    let qry = "PRAGMA table_info($#);"
+
+    check dbConn.getAllRows(sql qry % "String") == @[
+      @[?0, ?"s", ?"TEXT", ?1, ?nil, ?0],
+      @[?1, ?"sc10", ?"TEXT", ?1, ?nil, ?0],
+      @[?2, ?"psc5", ?"TEXT", ?1, ?nil, ?0],
+      @[?3, ?"id", ?"INTEGER", ?1, ?nil, ?1]
+    ]
+
+  test "Flavors of ``string``, insert row":
+    var str = newString("foo", newStringOfCap[10]("bar"), newPaddedStringOfCap[5]("baz"))
+
+    dbConn.insert(str)
+
+    check str.id > 0
+
+    let rows = dbConn.getAllRows(sql"""SELECT s, sc10, psc5, id FROM "String"""")
+
+    check rows.len == 1
+
+    check rows[0][0] == ?"foo"
+    check rows[0][1] == ?"bar"
+    check rows[0][2] == ?"baz  "
+    check rows[0][3] == ?str.id
+
+  test "Flavors of ``string``, get row":
+    var
+      inpString = newString("foo", newStringOfCap[10]("bar"), newPaddedStringOfCap[5]("baz"))
+      outString = newString()
+
+    with dbConn:
+      insert(inpString)
+      select(outString, "s = $1", "foo")
+
+    check inpString === outString
