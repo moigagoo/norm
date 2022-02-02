6fe1701648c227be550c84bdd13ba211b213e611
Clonkk <rf.clonk@linuxmail.org>
2022-02-02 14:31:49 +0100

    reformat test as std/unittest

--- tests/common/tpragmas_fix.nim
+++ tests/common/tpragmas_fix.nim
@@ -1,25 +1,44 @@
 import norm/pragmasutils
-template p() {.pragma.}
+import std/unittest
 
+template p() {.pragma.}
+template parg(s: string) {.pragma.}
 type
   A[T] = object
     a {.p.}: int
+    a2 {.parg: "Cat".}: int
 
   B = object
     b {.p.}: int
-  # Currently does not work : see PR https://github.com/nim-lang/Nim/pull/19451
-  C[T] = B
+    b2 {.parg: "Dog".}: int
+
+  C {.parg: "Mouse".} = B
 
 proc main() =
-  var a: A[int]
-  static:
-    doAssert a.a.hasCustomPragma(p)
-    doAssert not a.a.hasCustomPragma(A)
+  test "custom_pragma":
+    var a: A[int]
+    static:
+      doAssert a.a.hasCustomPragma(p)
+      doAssert not a.a.hasCustomPragma(A)
 
-  var b: B
-  static:
-    doAssert b.b.hasCustomPragma(p)
-    doAssert not b.b.hasCustomPragma(B)
-    doAssert not b.b.hasCustomPragma(C)
+    check a.a.hasCustomPragma(p)
+    check not a.a.hasCustomPragma(A)
+    check a.a2.getCustomPragmaVal(parg) == "Cat"
+
+    var b: B
+    static:
+      doAssert b.b.hasCustomPragma(p)
+      doAssert not b.b.hasCustomPragma(B)
+      doAssert not b.b.hasCustomPragma(C)
+
+    check b.b.hasCustomPragma(p)
+    check not b.b.hasCustomPragma(B)
+    check not b.b.hasCustomPragma(C)
+    check b.b2.getCustomPragmaVal(parg) == "Dog"
+    var c: C
+    check c.b2.getCustomPragmaVal(parg) == "Dog"
+    const x = C.getCustomPragmaVal(parg)
+    check x == "Mouse"
+
 
 main()
