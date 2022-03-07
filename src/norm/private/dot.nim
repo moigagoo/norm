7eb59b8ea6c9f576160630df0d4fdb7b9c363d40
Philipp Doerner <philipp3000doerner@web.de>
2022-03-07 20:55:43 +0100

    #127 unified "getField" macro into dot

--- src/norm/private/dot.nim
+++ src/norm/private/dot.nim
@@ -1,7 +1,7 @@
 import std/macros
 
 
-macro dot*(obj: ref object, fld: string): untyped =
+macro dot*(obj: typed, fld: string): untyped =
   ## Turn ``obj.dot("fld")`` into ``obj.fld``.
 
   newDotExpr(obj, newIdentNode(fld.strVal))
