7d422324b9dd83b1fc029d79fa4c429cc92b50e9
Philipp Doerner <philipp3000doerner@web.de>
2022-06-21 22:41:48 +0200

    Revert "##149 Added small additional piece of info for postgres"
    This reverts commit 98cd9b98a1527d060880c830f94ecbb73dd42686.

--- book/tutorial.nim
+++ book/tutorial.nim
@@ -12,7 +12,6 @@ Before going further, install [inim](https://github.com/inim-repl/INim) with nim
     $ nimble install -y inim
 
 Also, make sure you have SQLite installed. On most Linux distributions, it should be preinstalled. To install SQLite in macOS, use [brew](https://brew.sh/). On Windows, use [scoop](https://scoop.sh/).
-Should you want to follow this tutorial while using postgres, then please be aware that placeholders in postgres are not indicated with `?`, but with `$1`, `$2`, `$3`... etc. instead.
 
 Then, start a new inim session:
 
