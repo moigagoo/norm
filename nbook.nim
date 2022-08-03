d533c363b1721395bc2f4862a8167c75999c9e56
Constantine Molchanov <moigagoo@live.com>
2022-08-03 17:14:33 +0400

    Update docs command.

--- nbook.nim
+++ nbook.nim
@@ -1,7 +1,7 @@
 import nimibook
 
 
-var book = newBookFromToc("Norm: a Nim ORM", "book"):
+var book = initBookWithToc:
   entry("Welcome to Norm!", "index.nim")
   entry("Models 101", "models.nim")
   section("Tutorial", "tutorial.nim"):
@@ -15,10 +15,6 @@ var book = newBookFromToc("Norm: a Nim ORM", "book"):
   entry("Debugging SQL", "debug.nim")
   entry("Changelog", "changelog.nim")
 
-
-book.git_repository_url = "https://github.com/moigagoo/norm"
-book.favicon_escaped = """<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>🦾</text></svg>">"""
-book.preferred_dark_theme = "coal"
 
 nimibookCli(book)
 
