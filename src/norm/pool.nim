6cdf12fa72c5a1091c221a0f63d5a5d7bfaa2286
Constantine Molchanov <moigagoo@live.com>
2022-10-15 12:49:36 +0300

    Add doc comments.

--- src/norm/pool.nim
+++ src/norm/pool.nim
@@ -16,6 +16,13 @@ type
 
 
 func newPool*(defaultSize: Positive, poolExhaustedPolicy = pepRaise): Pool =
+  ##[ Create a connection pool of the given size.
+
+  ``poolExhaustedPolicy`` defines how the pool reacts when a connection is requested but the pool has no connection available:
+
+    - ``pepRaise`` (default) means throw ``PoolExhaustedError``
+    - ``pepExtend`` means throw “add another connection to the pool.”
+  ]##
   result = Pool(defaultSize: defaultSize, conns: newSeq[DbConn](defaultSize), poolExhaustedPolicy: poolExhaustedPolicy)
 
   initLock(result.lock)
@@ -30,6 +37,11 @@ func size*(pool: Pool): Natural =
   len(pool.conns)
 
 func pop*(pool: var Pool): DbConn =
+  ##[ Take a connection from the pool.
+
+  If you're calling this manually, don't forget to `add <#add>`_ it back!
+  ]##
+
   withLock(pool.lock):
     if pool.size > 0:
       result = pool.conns.pop()
@@ -41,16 +53,25 @@ func pop*(pool: var Pool): DbConn =
         result = getDb()
 
 func add*(pool: var Pool, dbConn: DbConn) =
+  ##[ Add a connection to the pool.
+
+  Use to return a borrowed connection to the pool.
+  ]##
+
   withLock(pool.lock):
     pool.conns.add(dbConn)
 
 func reset*(pool: var Pool) =
+  ## Reset pool size to ``defaultSize`` by closing and removing extra connections.
+
   withLock(pool.lock):
     while pool.size > pool.defaultSize:
       var conn = pool.conns.pop()
       close conn
 
 func close*(pool: var Pool) =
+  ## Close the pool by closing and removing all its connetions.
+
   withLock(pool.lock):
     for conn in pool.conns.mitems:
       close conn
@@ -58,6 +79,12 @@ func close*(pool: var Pool) =
     pool.conns.setLen(0)
 
 template withDb*(pool: var Pool, body: untyped): untyped =
+  ##[ Wrapper for DB operations.
+
+  Takes a ``DbConn`` from a pool as ``db`` variable,
+  runs your code in a ``try`` block, and returns connection to the pool afterward.
+  ]##
+
   block:
     let db {.inject.} = pool.pop()
 
