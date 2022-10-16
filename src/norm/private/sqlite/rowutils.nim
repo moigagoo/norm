c4798bea674251fd88151d7d056ee55a62dfe54f
Constantine Molchanov <moigagoo@live.com>
2022-10-16 13:57:24 +0300

    rowutils: Fix docstrings.

--- src/norm/private/sqlite/rowutils.nim
+++ src/norm/private/sqlite/rowutils.nim
@@ -21,9 +21,9 @@ func isEmptyColumn*(row: Row, index: int): bool =
 
 ## This does the actual heavy lifting for parsing
 proc fromRowPos[T: ref object](obj: var T, row: Row, pos: var Natural, skip: static bool = false) =
-  ##[ Convert ``ndb.sqlite.Row`` instance into `Model`_ instance, from a given position.
+  ##[ Convert ``ndb.sqlite.Row`` instance into a ``ref object`` instance, from a given position.
 
-  This is a helper proc to convert to `Model`_ instances that have fields of the same type.
+  This is a helper proc to convert to ``ref object`` instances that have fields of the same type.
   ]##
 
   for fld, dummyVal in T()[].fieldPairs:
@@ -31,10 +31,10 @@ proc fromRowPos[T: ref object](obj: var T, row: Row, pos: var Natural, skip: sta
       if dot(obj, fld).toOptional().isSome:             ## and it's either a ``some ref object`` or ``ref object``
         var subMod = dot(obj, fld).toOptional().get()   ## then we try to populate it with the next ``row`` values.
 
-        if row.isEmptyColumn(pos):                      ## If we have a ``NULL`` at this point, we return an empty ``Model``:
-          when typeof(dummyVal) is Option:              ## ``val`` is guaranteed to be either ``Model`` or an ``Option[Model]`` at this point,
+        if row.isEmptyColumn(pos):                      ## If we have a ``NULL`` at this point, we return an empty instance:
+          when typeof(dummyVal) is Option:              ## ``val`` is guaranteed to be either ``ref object `` or an ``Option[ref object]`` at this point,
             when isRefObject(dummyVal):
-              dot(obj, fld) = none typeof(subMod)       ## and the fact that we got a ``NULL`` tells us it's an ``Option[Model]``,
+              dot(obj, fld) = none typeof(subMod)       ## and the fact that we got a ``NULL`` tells us it's an ``Option[ref object]``,
 
           inc pos
           subMod.fromRowPos(row, pos, skip = true)      ## Then we skip all the ``row`` values that should've gone into this submodel.
@@ -55,9 +55,9 @@ proc fromRowPos[T: ref object](obj: var T, row: Row, pos: var Natural, skip: sta
         inc pos
   
 proc fromRow*[T: ref object](obj: var T, row: Row) =
-  ##[ Populate `Model`_ instance from ``ndb.sqlite.Row`` instance.
+  ##[ Populate ``ref object`` instance from ``ndb.sqlite.Row`` instance.
 
-  Nested `Model`_ fields are populated from the same ``ndb.sqlite.Row`` instance.
+  Nested ``ref object`` fields are populated from the same ``ndb.sqlite.Row`` instance.
   ]##
 
   var pos: Natural = 0
